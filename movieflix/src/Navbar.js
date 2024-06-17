0e6e4c5e8388198149d7aa5aac61eeab37980dff
meghna7838 <meghnatandon7838@gmail.com>
2024-06-17 19:51:54 +0530

    Navbar internal styling

--- movieflix/src/Navbar.js
+++ movieflix/src/Navbar.js
@@ -5,17 +5,11 @@ class Navbar extends React.Component{
     {
         return(
             <>
-            <div style={
-                {width:"100%",
-                height:70,
-                background:"red",
-                display:"flex",
-                justifyContent:"space-between"
-            }}>
-            <div>Title</div>
-            <div>
-            <img alt="Cart"/>
-            <span>0</span>  
+            <div style={style.nav}>
+            <div style={style.title}>Movie-App</div>
+            <div style={style.cartContainer}>
+            <img src="https://cdn-icons-png.flaticon.com/128/891/891462.png" style={style.cartIcon}/>
+            <span style={style.cartCount}>0</span>  
             </div>
             </div>
             </>
@@ -23,4 +17,42 @@ class Navbar extends React.Component{
     }
 }
 
+const style = {
+    nav : 
+    {
+    height:70,
+    background:"#4267b2",
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center",
+    position:"relative"
+    },
+    title:{
+        fontSize:30,
+        color:"#fff",
+        fontWeight:600,
+        fontFamily:'"Montserrat",sans-serif',
+        textTransform:"uppercase",
+        marginLeft:20
+    },
+    cartContainer:{
+        position:"relative",
+        cursor:"pointer",
+    },
+    cartIcon:{
+        height:48,
+        marginRight:20
+    },
+    cartCount:{
+        background:"orange",
+        borderRadius:"50%",
+        padding:"4px 8px",
+        position:"absolute",
+        right:10,
+        top:-5,
+        fontSize:12
+    }
+
+}
 export default Navbar;
+
