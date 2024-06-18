c6f81beab6eb68b524cc5a98a89c9e9cb7dabc67
meghna7838 <meghnatandon7838@gmail.com>
2024-06-18 10:12:29 +0530

    CSS Module

--- movieflix/src/Navbar.js
+++ movieflix/src/Navbar.js
@@ -1,48 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import navstyle from "./Navbar.module.css";
 
-const Nav = styled.div `
-width:100%;
-height:70px;
-background:linear-gradient(170deg, #1bc059,#0d47a1);
-display:flex;
-justify-content:space-between;
-align-items:center;
-position:relative
+// const Nav = styled.div `
+// width:100%;
+// height:70px;
+// background:linear-gradient(170deg, #1bc059,#0d47a1);
+// display:flex;
+// justify-content:space-between;
+// align-items:center;
+// position:relative
 
-`;
-const Title= styled.div`
-    font-size:40px;
-    color:#fff;
-    font-weight:600;
-    font-family:'Montserrat,sans-serif';
-    text-transform:uppercase;
-    margin-left:20px;
-    &:hover{color: #0f0}
+// `;
+// const Title= styled.div`
+//     font-size:40px;
+//     color:#fff;
+//     font-weight:600;
+//     font-family:'Montserrat,sans-serif';
+//     text-transform:uppercase;
+//     margin-left:20px;
+//     &:hover{color: #0f0}
     
-    `;
+//     `;
 
-const Cartcontainer = styled.div`
-cursor:pointer;
-position:relative;
-`;
+// const Cartcontainer = styled.div`
+// cursor:pointer;
+// position:relative;
+// `;
 
-const Carticon = styled.img`
-margin-right:20px;
-margin-top:5px;
-height:40px;
-`;
+// const Carticon = styled.img`
+// margin-right:20px;
+// margin-top:5px;
+// height:40px;
+// `;
 
-const Cartcount = styled.div`
-background-color:${(props)=>props.color};
-border-radius:50%;
-padding:4px 8px;
-position:absolute;
-right:10px;
-top:-5px;
-font-size:12px;
-visibility:${(props)=>props.show?"visible":"hidden"}
-`
+// const Cartcount = styled.div`
+// background-color:${(props)=>props.color};
+// border-radius:50%;
+// padding:4px 8px;
+// position:absolute;
+// right:10px;
+// top:-5px;
+// font-size:12px;
+// visibility:${(props)=>props.show?"visible":"hidden"}
+// `
 
 class Navbar extends React.Component{
 
@@ -50,13 +51,13 @@ class Navbar extends React.Component{
     {
         return(
             <>
-            <Nav>
-            <Title>Movie-App</Title>
-            <Cartcontainer>
-            <Carticon src="https://cdn-icons-png.flaticon.com/128/891/891462.png" />
-            <Cartcount color="yellow" show={true}>0</Cartcount>  
-            </Cartcontainer>
-            </Nav>
+            <div className={navstyle.nav}>
+            <div className={navstyle.title}>Movie-App</div>
+            <div className={navstyle.cartcontainer}>
+            <img className={navstyle.carticon} src="https://cdn-icons-png.flaticon.com/128/891/891462.png" />
+            <div className={navstyle.cartcount} >0</div>  
+            </div>
+            </div>
             </>
         )
     }
