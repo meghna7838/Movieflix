87fdf0e392449998dfa2665a1535f451a53ec6e0
meghna7838 <meghnatandon7838@gmail.com>
2024-06-18 17:23:16 +0530

    Moving state to parent so that the props can be passed to both the children of app

--- movieflix/src/App.js
+++ movieflix/src/App.js
@@ -1,15 +1,74 @@
 import React from "react";
 import Movielist from "./Movielist";
 import Navbar from "./Navbar";
+import {movies} from "./MoviesData"
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+        movies : movies,
+        cartCount:3
+    }
+    }
+
+      handleincStar = (movie) =>{
+        const {movies} = this.state;
+        const mid = movies.indexOf(movie);
+        if(movies.star >=5)
+          {
+            return;
+          }
+        movies[mid].star +=0.5;
+        this.setState({
+          movies:movies
+        })
+      }
+
+      decreasestar = (movie) =>{
+        const {movies} = this.state;
+        const mid = movies.indexOf(movie);
+        if(movies[mid].star<=0)
+          {
+            return;
+          }
+        movies[mid].star -=0.5;
+        this.setState({
+          movies:movies
+        })
+      }
+      handleClickFav = (movie) =>{
+        const {movies} = this.state;
+        const mid = movies.indexOf(movie);
+        movies[mid].fav=!movies[mid].fav;
+          this.setState({
+        movies:movies
+        })
+        
+      }
+
+      handleClickCart = (movie) =>{
+        const {movies} = this.state;
+        const mid = movies.indexOf(movie);
+        movies[mid].cart= !movies[mid].cart;
+          this.setState({
+            movies
+          })
+      }
+
 
   render(){
+
     return (
       <>
       <Navbar/>
         <h1>Movieflix</h1>
-        <Movielist/>
+        <Movielist movies={movies} 
+        handleincStar={this.handleincStar}
+        decreasestar={this.decreasestar}
+        handleClickFav={this.handleClickFav}
+        handleClickCart={this.handleClickCart}
+         />
       </>
     );
   }
